15235b186d8e8defffef94bdb160c9886fc90ed1
Milan Surelia <94677877+milan-7span@users.noreply.github.com>
2022-10-26 17:49:38 +0530

    Added Custom `search_delegate` Snippet

---
+++ snippets/search_delegate.dart
@@ -0,0 +1,131 @@
+# Search Delegate
+
+Code:
+```
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final List<String> list = List.generate(10, (index) => "Text $index");
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {
+              showSearch(context: context, delegate: Search(widget.list));
+            },
+            icon: const Icon(Icons.search),
+          )
+        ],
+        centerTitle: true,
+        title: const Text('Search Bar'),
+      ),
+      body: ListView.builder(
+        itemCount: widget.list.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text(
+            widget.list[index],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Search extends SearchDelegate {
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return <Widget>[
+      IconButton(
+        icon: const Icon(Icons.close),
+        onPressed: () {
+          query = "";
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+  }
+
+  String selectedResult = "";
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return 
+       Center(
+        child: Text(selectedResult),
+      
+    );
+  }
+
+  final List<String> listExample;
+  Search(this.listExample);
+
+  List<String> recentList = ["Text 4", "Text 3"];
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> suggestionList = [];
+    query.isEmpty
+        ? suggestionList = recentList //In the true case
+        : suggestionList.addAll(listExample.where(
+            // In the false case
+            (element) => element.contains(query),
+          ));
+
+    return ListView.builder(
+      itemCount: suggestionList.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(
+            suggestionList[index],
+          ),
+          leading: query.isEmpty ? const Icon(Icons.access_time) : const SizedBox(),
+          onTap: (){
+            selectedResult = suggestionList[index];
+            showResults(context);
+          },
+        );
+      },
+    );
+  }
+}
+```
+
+## Try out on [Dartpad](https://dartpad.dev/?id=a0cde91e2b6c013f345c61dd3cfb86dd). 
